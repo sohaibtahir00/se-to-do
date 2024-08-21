f18a03adf298992b3962d970699747cd90b94b88
Sohaib Tahir <sohaibtahir90@gmail.com>
2024-08-21 09:09:18 -0500

    Project 7

---
+++ pages/index.js
@@ -0,0 +1,56 @@
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
+import { initialTodos, validationConfig } from "../utils/constants.js";
+import Todo from "../components/Todo.js";
+import FormValidator from "../components/FormValidator.js";
+
+const addTodoButton = document.querySelector(".button_action_add");
+const addTodoPopup = document.querySelector("#add-todo-popup");
+const addTodoForm = addTodoPopup.querySelector(".popup__form");
+const addTodoCloseBtn = addTodoPopup.querySelector(".popup__close");
+const todosList = document.querySelector(".todos__list");
+
+const openModal = (modal) => {
+  modal.classList.add("popup_visible");
+};
+
+const closeModal = (modal) => {
+  modal.classList.remove("popup_visible");
+};
+
+function generateTodo(data) {
+  const todo = new Todo(data, "#todo-template");
+  return todo.getView();
+}
+
+addTodoButton.addEventListener("click", () => {
+  openModal(addTodoPopup);
+});
+
+addTodoCloseBtn.addEventListener("click", () => {
+  closeModal(addTodoPopup);
+});
+
+addTodoForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  const name = evt.target.name.value;
+  const dateInput = evt.target.date.value;
+
+  const date = new Date(dateInput);
+  date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
+
+  const id = uuidv4();
+  const values = { name, date, id };
+  const todo = generateTodo(values);
+  todosList.append(todo);
+  closeModal(addTodoPopup);
+
+  addTodoForm.reset();
+});
+
+initialTodos.forEach((item) => {
+  const todo = generateTodo(item);
+  todosList.append(todo);
+});
+
+const todoNewValidator = new FormValidator(validationConfig, addTodoForm);
+todoNewValidator.enableValidation();
